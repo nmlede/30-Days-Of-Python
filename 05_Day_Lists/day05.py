27b97fd905b566523e9623adae5a7d854f14506d
Nicolas <nmlede1988@gmail.com>
2022-05-07 16:53:21 -0300

    Dia 5 terminado

--- 05_Day_Lists/day05.py
+++ 05_Day_Lists/day05.py
@@ -2,13 +2,8 @@
 
 # Exercises: Level 1
 # 1 Declare an empty list
-from __future__ import print_function
-import re
-from signal import ITIMER_REAL
-from types import coroutine
-from xml.sax.handler import property_interning_dict
-
-from numpy import irr, sort
+from itertools import count
+from struct import unpack
 
 
 lst_vacia=list()
@@ -68,7 +63,7 @@ print('Oracle' in it_companies)
 print('Morcilla' in it_companies)
 
 # 16 Sort the list using sort() method
-print(sort(it_companies)) # Ordena alfabeticamente.
+print(it_companies.sort()) # Ordena alfabeticamente.
 
 # 17 Reverse the list in descending order using reverse() method
 it_companies.reverse() # Invierte el orden
@@ -85,31 +80,109 @@ print(it_companies[:-3])
 # 20 Slice out the middle IT company or companies from the list
 print(it_companies[len(it_companies)//2])
 
-# Remove the first IT company from the list
+# 21 Remove the first IT company from the list
+print(it_companies)
+print(it_companies.pop(0))
+print(it_companies)
 
-# Remove the middle IT company or companies from the list
+# 22 Remove the middle IT company or companies from the list
+print(it_companies)
+print(it_companies.pop(len(it_companies)//2))
+print(it_companies)
 
-# Remove the last IT company from the list
+# 23 Remove the last IT company from the list
+print(it_companies)
+print(it_companies.pop(len(it_companies)-1))
+print(it_companies)
 
-# Remove all IT companies from the list
+# 24 Remove all IT companies from the list
+it_companies.clear()
+print(it_companies)
 
-# Destroy the IT companies list
+# 25 Destroy the IT companies list
+del it_companies
 
-# Join the following lists:
-
+# 26 Join the following lists:
 # front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
 # back_end = ['Node','Express', 'MongoDB']
-# After joining the lists in question 26. Copy the joined list and assign it to a variable full_stack. Then insert Python and SQL after Redux.
+# After joining the lists in question 26. Copy the joined list and assign it to a variable 
+# full_stack. Then insert Python and SQL after Redux.
+front_end=['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end=['Node','Express', 'MongoDB']
+front_end=front_end+back_end # Join es concatenar listas
+print(front_end)
+
+full_stack=front_end.copy() # copia una lista/array
+print(full_stack)
+
+
 
 # Exercises: Level 2
 # The following is a list of 10 students ages:
-# ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
-# Sort the list and find the min and max age
-# Add the min age and the max age again to the list
-# Find the median age (one middle item or two middle items divided by two)
-# Find the average age (sum of all items divided by their number )
-# Find the range of the ages (max minus min)
-# Compare the value of (min - average) and (max - average), use abs() method
-# Find the middle country(ies) in the countries list
-# Divide the countries list into two equal lists if it is even if not one more country for the first half.
-# ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']. Unpack the first three countries and the rest as scandic countries.
+ages=[19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+# 1 Sort the list and find the min and max age
+ages.sort() # la lista se ordena y se almacena en la misma variable original
+print(ages)
+print(f'La edad menor es: {ages[0]}, y la edad mayor es: {ages[-1]}')
+
+def find_ages(lista):
+    min_age=lista[0]
+    max_age=lista[0]
+    for idx in range(1, len(lista)):
+        if lista[idx-1] > lista[idx]: # Usar idx-1 para no tener error de out of range
+            min_age=lista[idx]
+        if lista[idx-1] < lista[idx]:
+            max_age=lista[idx]
+    
+    return min_age,max_age # retorna dos valores
+
+min_age,max_age=find_ages(ages) # declaro dos variables a las que se les asignan
+# los valores retornados en orden.
+
+print(f'La menor edad es: {min_age}, y la mayor edad es: {max_age}')
+
+# 2 Add the min age and the max age again to the list
+ages.append(min_age)
+ages.append(max_age)
+print(f'Lista con elementos agregados {ages}')
+
+# 3 Find the median age (one middle item or two middle items divided by two)
+# INVESTIGAR A QUE SE REFIERE EL PUNTO
+
+# 4 Find the average age (sum of all items divided by their number) PROMEDIO
+ages.sort()
+sum_ages=sum(ages)
+prom_ages=sum_ages//len(ages)
+print(f'El promedio de edad es de: {prom_ages}')
+
+# 5 Find the range of the ages (max minus min)
+range_age=max_age-min_age
+print(f'El rango de edad es de {range_age}')
+
+# 6 Compare the value of (min - average) and (max - average), use abs() method (VALOR ABSOLUTO)
+comp_min=abs(min_age-prom_ages)
+comp_max=abs(max_age-prom_ages)
+print('True' if comp_min==comp_max else 'False') # Comparo usando ternarios (muy util)
+
+# 7 Find the middle country(ies) in the countries list
+countries=['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']
+
+middle_country_index=len(countries)//2
+print(countries[middle_country_index])
+
+# 8 Divide the countries list into two equal lists if it is even if not one more 
+# country for the first half.
+half_list_a=countries[0:middle_country_index+1]
+print(half_list_a)
+half_list_b=countries[middle_country_index+1:]
+print(half_list_b)
+
+# 9 Unpack the first three countries and the rest as scandic countries.
+# El asterisco indica que todos los siguientes valores se asignan a una misma variable
+# que va a ser una nueva lista.
+countrie_a,countrie_b,countrie_c,*scandic_countries=countries 
+print(countrie_a)
+print(countrie_b)
+print(countrie_c)
+print(scandic_countries)
